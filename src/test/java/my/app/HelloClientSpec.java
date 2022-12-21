aa4f4cb9758738662734e7996904a0223b18cc5b
Ivett BOP <isoti@billionoysterproject.onmicrosoft.com>
2022-12-21 12:27:33 -0800

    Initial setup

---
+++ src/test/java/my/app/HelloClientSpec.java
@@ -0,0 +1,22 @@
+package my.app;
+
+import hello.world.HelloClient;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import jakarta.inject.Inject;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+public class HelloClientSpec  {
+
+    @Inject
+    HelloClient client;
+
+    @Test
+    public void testHelloWorldResponse(){
+        assertEquals("Hello World", Mono.from(client.hello()).block());
+    }
+}
